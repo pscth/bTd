aa7a36b07170096864b63530f12a8512a2443f3e
pscth <dragonfistexplode@gmail.com>
2014-01-15 02:45:29 -0800

    script pulga controler con funciones basicas. aplicado a forzuda. es clase abstracta!! pero los metodos no los sobrescribo, solo uso la clase para conservar variables y métodos comunes.

--- _Scripts/btdPulgaForzuda.cs
+++ _Scripts/btdPulgaForzuda.cs
@@ -1,19 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class btdPulgaForzuda : MonoBehaviour
+
+public class btdPulgaForzuda : btdPulgaController
 {
-    public float movementSpeed;
-    public float jumphight;
-
-    private int pullDirection;
-
-    public int state;
 	public int pullstate;
-	public int pullAngle;
-	
-    private int selected;
-	
+	public int pullAngle;	   
+
 	private GameObject LHand; //brazos
 	private GameObject RHand;
 	public GameObject Grabber;
@@ -22,13 +15,8 @@ public class btdPulgaForzuda : MonoBehaviour
 
     // Use this for initialization
     void Start()
-    {
-		// Common
-        movementSpeed = btdConstants.MOVE_SPEED;
-        jumphight = btdConstants.JUMP_HIGHT;
-        rigidbody.mass = btdConstants.PULGA_MASS;
-        selected = btdConstants.PULGA_UNSELECTED;
-        state = btdConstants.PULGA_WAIT;
+    {        
+		PulgaCommonStart ();
 		// Forzude
 		pullstate = btdConstants.PULGA_FORZUDE_NO_PULL;
 		LHand =GameObject.Find ("LArm");
@@ -42,19 +30,8 @@ public class btdPulgaForzuda : MonoBehaviour
 	void Update ()
     {
         if (selected == btdConstants.PULGA_SELECTED)
-        {
-            // Movimiento lateral ----
-       		float forward = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
-			if(forward>0) transform.localRotation = Quaternion.Euler(0,0,0);
-			if(forward<0) {transform.localRotation = Quaternion.Euler(0,180,0);  forward=-forward;}
-        	transform.Translate(Vector3.right * forward);
-			
-            //Salto
-			if (Input.GetButton("Jump") && state != btdConstants.PULGA_JUMP)				
-            {
-                rigidbody.AddForce(Vector3.up * jumphight);
-                state = btdConstants.PULGA_JUMP;
-            }
+        {         
+			PulgaCommonUpdate ();
 			//Coger ------
 			if (Input.GetButton("Fire1") && pullstate==btdConstants.PULGA_FORZUDE_NO_PULL && state==btdConstants.PULGA_FORZUDE_PULLUP) 
         	{  					
@@ -115,42 +92,14 @@ public class btdPulgaForzuda : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter(Collider other)
+	void OnTriggerEnter(Collider other)
     {
+		PulgaCommonOnTriggerEnter (other);
 		switch(other.tag)
 		{
-			//------------------------------------------------------------
-			case "ground":
-				if (state == btdConstants.PULGA_JUMP)
-           		{
-              	   state = btdConstants.PULGA_WAIT;
-           	 	}
-            	else if (state == btdConstants.PULGA_FORZUDE_PULLUP_BUT_JUMPING)
-            	{
-                   state = btdConstants.PULGA_FORZUDE_PULLUP;
-                   renderer.material.color = Color.yellow;
-            	}
-            	Debug.Log("Ground");
-				break;
-			//------------------------------------------------------------
-			/*case "pullUpLeft":
-				pullDirection = btdConstants.LEFT;
-            	state = btdConstants.PULGA_FORZUDE_PULLUP;
-            	renderer.material.color = Color.yellow;
-            	Debug.Log("PullLeft");
-				break;
-			//------------------------------------------------------------
-			case "PullUpRight":
-				pullDirection = btdConstants.RIGHT;
-            	state = btdConstants.PULGA_FORZUDE_PULLUP;
-            	renderer.material.color = Color.yellow;
-            	Debug.Log("PullRight");
-				break;*/	
-			//------------------------------------------------------------
 			case "PullUpObj":		
 				if(pullstate==btdConstants.PULGA_FORZUDE_NO_PULL)
 				{
-					pullDirection = btdConstants.CENTER;
 					state = btdConstants.PULGA_FORZUDE_PULLUP;
             		renderer.material.color = Color.yellow;
 					Pullable=GameObject.Find(other.gameObject.name);
@@ -164,18 +113,13 @@ public class btdPulgaForzuda : MonoBehaviour
    
     }
 
-    void OnTriggerExit(Collider other)
-    {
-       // if (other.tag == "pullUpRight" || other.tag == "pullUpLeft")
+	void OnTriggerExit(Collider other)
+    {       
+		PulgaCommonOnTriggerExit (other);
         if (other.tag == "PullUpObj" )
 		{
             renderer.material.color = Color.green;
 			state = btdConstants.PULGA_WAIT;
         }
-    }
-
-    public void setSelected(int _selected)
-    {
-        selected = _selected;
     }
 }
